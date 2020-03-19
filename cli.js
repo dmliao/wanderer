a260d20b41c91e720063fff0ec275323049f34a8
Amorphous <dl.amorphous@gmail.com>
2020-03-18 23:00:25 -0700

    More prettier

--- cli.js
+++ cli.js
@@ -1,17 +1,19 @@
 #! /usr/bin/env node
 
-const toml = require('@iarna/toml');
-const path = require('upath');
-const fs = require('fs');
+const toml = require('@iarna/toml')
+const path = require('upath')
+const fs = require('fs')
 
-var argv = require('minimist')(process.argv.slice(2));
-const wanderer = require('./index');
+var argv = require('minimist')(process.argv.slice(2))
+const wanderer = require('./index')
 
-console.time('build');
+console.time('build')
 
 if (argv.h || argv.help) {
-    console.log('Usage: wanderer -f <frame directory> -i <content directory> -o <out directory> -c <config file>')
-    return;
+	console.log(
+		'Usage: wanderer -f <frame directory> -i <content directory> -o <out directory> -c <config file>'
+	)
+	return
 }
 
 const frameDir = argv.frame || argv.f || path.resolve(process.cwd(), 'frame')
@@ -19,37 +21,40 @@ const contentDir = argv.in || argv.i || path.resolve(process.cwd(), 'content')
 const buildDir = argv.out || argv.o || path.resolve(process.cwd(), 'build')
 const cacheDir = argv.cache || argv.a || path.resolve(process.cwd(), '.cache')
 
-let metaConfig = {
-};
+let metaConfig = {}
 
-const configFile = argv.config || argv.c || path.resolve(process.cwd(), 'config.toml')
+const configFile =
+	argv.config || argv.c || path.resolve(process.cwd(), 'config.toml')
 if (fs.existsSync(configFile)) {
-    const extraConfig = toml.parse(fs.readFileSync(path.resolve(process.cwd(), 'config.toml')));
-    metaConfig = {...metaConfig, ...extraConfig}
+	const extraConfig = toml.parse(
+		fs.readFileSync(path.resolve(process.cwd(), 'config.toml'))
+	)
+	metaConfig = { ...metaConfig, ...extraConfig }
 }
 
 if (argv.clean) {
-    // clean up cache stuff
-    try {
-        fs.unlinkSync(path.resolve(cacheDir, 'static', 'pages.json'))
-    } catch(e) {
-        // swallow it
-    }
+	// clean up cache stuff
+	try {
+		fs.unlinkSync(path.resolve(cacheDir, 'static', 'pages.json'))
+	} catch (e) {
+		// swallow it
+	}
 
-    try {
-        fs.unlinkSync(path.resolve(cacheDir, 'pages.json'))
-    } catch (e) {
-        // swallow it
-    }
+	try {
+		fs.unlinkSync(path.resolve(cacheDir, 'pages.json'))
+	} catch (e) {
+		// swallow it
+	}
 
-    try {
-        fs.unlinkSync(path.resolve(cacheDir, '.touchfile'))
-    } catch (e) {
-        // swallow it
-    }
-    
-    fs.rmdirSync(buildDir, { recursive: true })
+	try {
+		fs.unlinkSync(path.resolve(cacheDir, '.touchfile'))
+	} catch (e) {
+		// swallow it
+	}
+
+	fs.rmdirSync(buildDir, { recursive: true })
 }
 
-wanderer(metaConfig, frameDir, contentDir, cacheDir, buildDir).then(
-    () => console.timeEnd('build'))
+wanderer(metaConfig, frameDir, contentDir, cacheDir, buildDir).then(() =>
+	console.timeEnd('build')
+)
--- tools/touch/touch.js
+++ tools/touch/touch.js
@@ -1,82 +1,110 @@
-const fs = require('fs');
-const path = require('upath');
-const dayjs = require('dayjs');
-const toml = require('@iarna/toml');
+const fs = require('fs')
+const path = require('upath')
+const dayjs = require('dayjs')
+const toml = require('@iarna/toml')
 
 const processTempoFilename = require('../tempo/process-tempo-filename')
 
+const createTouchedFileObject = (
+	isTouched,
+	baseDir,
+	dir,
+	file,
+	stats,
+	fileConfig
+) => {
+	// do first pass of processing since touch is the only time we traverse the directory
+	// and touch all the files
+	const id = path.relative(baseDir, path.resolve(dir, file))
+	const pageTempo = processTempoFilename(path.parse(file).name)
 
-const createTouchedFileObject = (isTouched, baseDir, dir, file, stats, fileConfig) => {
-    // do first pass of processing since touch is the only time we traverse the directory
-    // and touch all the files
-    const id = path.relative(baseDir, path.resolve(dir, file))
-    const pageTempo = processTempoFilename(path.parse(file).name)
+	const newConfig = { ...fileConfig }
 
-    const newConfig = { ...fileConfig }
+	if (pageTempo.date) {
+		newConfig.date = pageTempo.date
+	} else {
+		newConfig.date = stats.mtime
+	}
 
-    if (pageTempo.date) {
-        newConfig.date = pageTempo.date
-    } else {
-        newConfig.date = stats.mtime
-    }
-
-    newConfig.updated = stats.mtime
-    newConfig.pageName = pageTempo.name
-    return {
-        isModified: isTouched,
-        id,
-        file,
-        dir: path.resolve(dir),
-        config: newConfig,
-    }
+	newConfig.updated = stats.mtime
+	newConfig.pageName = pageTempo.name
+	return {
+		isModified: isTouched,
+		id,
+		file,
+		dir: path.resolve(dir),
+		config: newConfig
+	}
 }
 
 const touchRecursive = async (baseDir, dir, globalConfig, dateToCheck) => {
-    let fileConfig = globalConfig || {}
-    let files = fs.readdirSync(dir);
-    files = await Promise.all(files.map(async file => {
-        const filePath = path.join(dir, file);
-        const stats = fs.statSync(filePath);
+	let fileConfig = globalConfig || {}
+	let files = fs.readdirSync(dir)
+	files = await Promise.all(
+		files.map(async (file) => {
+			const filePath = path.join(dir, file)
+			const stats = fs.statSync(filePath)
 
-        // find the directory-level config
-        const configFilePath = path.resolve(dir, '_.toml')
-        if (fs.existsSync(configFilePath)) {
-            const dirConfig = toml.parse(fs.readFileSync(configFilePath).toString())
-            fileConfig = { ...globalConfig, ...dirConfig }
-        }
+			// find the directory-level config
+			const configFilePath = path.resolve(dir, '_.toml')
+			if (fs.existsSync(configFilePath)) {
+				const dirConfig = toml.parse(
+					fs.readFileSync(configFilePath).toString()
+				)
+				fileConfig = { ...globalConfig, ...dirConfig }
+			}
 
-        if (fileConfig.private && fileConfig.private === true) {
-            return;
-        }
+			if (fileConfig.private && fileConfig.private === true) {
+				return
+			}
 
-        if (stats.isDirectory()) return touchRecursive(baseDir, filePath, fileConfig, dateToCheck);
-        else if (stats.isFile()) {
-            if (dayjs(dateToCheck).isBefore(dayjs(stats.mtime))) {
-                // this was touched!
+			if (stats.isDirectory())
+				return touchRecursive(
+					baseDir,
+					filePath,
+					fileConfig,
+					dateToCheck
+				)
+			else if (stats.isFile()) {
+				if (dayjs(dateToCheck).isBefore(dayjs(stats.mtime))) {
+					// this was touched!
 
-                return createTouchedFileObject(true, baseDir, dir, file, stats, fileConfig)
-            }
+					return createTouchedFileObject(
+						true,
+						baseDir,
+						dir,
+						file,
+						stats,
+						fileConfig
+					)
+				}
 
-            return createTouchedFileObject(false, baseDir, dir, file, stats, fileConfig);
-        }
-    }));
+				return createTouchedFileObject(
+					false,
+					baseDir,
+					dir,
+					file,
+					stats,
+					fileConfig
+				)
+			}
+		})
+	)
 
-    return files
+	return files
 }
 
 const touch = async (dir, globalConfig, dateToCheck) => {
+	const files = await touchRecursive(dir, dir, globalConfig, dateToCheck)
+	const flats = files.flat(Infinity)
+	const processedFiles = []
+	for (let file of flats) {
+		if (file) {
+			processedFiles.push(file)
+		}
+	}
 
-    const files = await touchRecursive(dir, dir, globalConfig, dateToCheck)
-    const flats = files.flat(Infinity);
-    const processedFiles = [];
-    for (let file of flats) {
-        if (file) {
-            processedFiles.push(file)
-        }
-    }
-
-    return processedFiles;
+	return processedFiles
 }
 
-
-module.exports = touch;
+module.exports = touch
