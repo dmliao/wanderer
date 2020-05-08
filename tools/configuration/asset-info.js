31de17e6313633d0c47d39c47624b1e3a5d61c35
DM Liao <dl.amorphous@gmail.com>
2020-05-08 02:47:58 -0400

    Add next and previous page generation to asset info

--- tools/configuration/asset-info.js
+++ tools/configuration/asset-info.js
@@ -4,6 +4,7 @@ const getConfiguration = require('./configuration')
 
 const processTempoFilename = require('../tempo/process-tempo-filename')
 
+// an asset can be any file
 const getAssetInfo = (baseDir, targetFile) => {
 	const targetFilePath = path.resolve(targetFile);
 	const stats = fs.statSync(targetFilePath);
@@ -17,6 +18,7 @@ const getAssetInfo = (baseDir, targetFile) => {
 	} else {
 		date = stats.mtime
 	}
+
 	const updated = stats.mtime;
 	const pageName = pageTempo.name;
 
@@ -31,9 +33,18 @@ const getAssetInfo = (baseDir, targetFile) => {
 		pathEnd === 'index' ? '' : pathEnd
 	].join('/')
 
+	if (!url.startsWith('/')) {
+		url = '/' + url
+	}
+
+	const dir = path.dirname(targetFilePath);
+	const file = path.basename(targetFilePath);
+
 	const assetInfo = {
 		path: targetFilePath,
 		id,
+		dir, 
+		file,
 		date,
 		updated,
 		pageName,
@@ -44,6 +55,8 @@ const getAssetInfo = (baseDir, targetFile) => {
 	return assetInfo;
 }
 
+// a page specifically is a content file, so adds 
+// additional helpers like a title and the body text
 const getPageInfo = (baseDir, targetFile) => {
 	const assetInfo = getAssetInfo(baseDir, targetFile);
 	const assetText = fs.readFileSync(path.resolve(targetFile), 'utf-8');
@@ -59,7 +72,7 @@ const getPageInfo = (baseDir, targetFile) => {
 			.slice(2)
 			.trim()
 	} else {
-		title = assetInfo.config.pageName
+		title = assetInfo.pageName
 	}
 
 	return {
@@ -69,7 +82,60 @@ const getPageInfo = (baseDir, targetFile) => {
 	}
 }
 
+// previous and next pages are determined by their alphabetical order in a directory.
+// note that you can number pages to ensure that they order properly
+// the predicate takes an assetInfo as input, and returns true if it's a valid page
+// this is a way to skip files that aren't pages, or index files.
+// example: 01-title.md
+const getPreviousPage = (baseDir, targetFile, predicate) => {
+	const assetInfo = getAssetInfo(baseDir, targetFile);
+
+	const possiblePages = fs.readdirSync(assetInfo.dir);
+	const startIndex = possiblePages.indexOf(assetInfo.file);
+
+	for (let i = startIndex - 1; i >= 0; i--) {
+		const possibleFilename = possiblePages[i];
+		const possibleFilePath = path.resolve(assetInfo.dir, possibleFilename);
+		const newAssetInfo = getAssetInfo(baseDir, possibleFilePath);
+		if (predicate(newAssetInfo)) {
+			const newPageInfo = getPageInfo(baseDir, possibleFilePath);
+			return {
+				id: newPageInfo.id,
+				title: newPageInfo.title,
+				url: newPageInfo.url,
+			}
+		}
+	}
+
+	return undefined;
+}
+
+const getNextPage = (baseDir, targetFile, predicate) => {
+	const assetInfo = getAssetInfo(baseDir, targetFile);
+
+	const possiblePages = fs.readdirSync(assetInfo.dir);
+	const startIndex = possiblePages.indexOf(assetInfo.file);
+
+	for (let i = startIndex + 1; i < possiblePages.length; i++) {
+		const possibleFilename = possiblePages[i];
+		const possibleFilePath = path.resolve(assetInfo.dir, possibleFilename);
+		const newAssetInfo = getAssetInfo(baseDir, possibleFilePath);
+		if (predicate(newAssetInfo)) {
+			const newPageInfo = getPageInfo(baseDir, possibleFilePath);
+			return {
+				id: newPageInfo.id,
+				title: newPageInfo.title,
+				url: newPageInfo.url,
+			}
+		}
+	}
+
+	return undefined;
+}
+
 module.exports = { 
 	getAssetInfo,
 	getPageInfo,
+	getPreviousPage,
+	getNextPage,
 }
