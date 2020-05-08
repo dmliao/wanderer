d3e90c82ddb756a503b1a92ee82512c41445fa2a
DM Liao <dl.amorphous@gmail.com>
2020-05-08 02:14:03 -0400

    Remove touch dependency on configuration

--- tools/configuration/configuration.spec.js
+++ tools/configuration/configuration.spec.js
@@ -1,12 +1,9 @@
-const touch = require('../touch/touch')
 const getConfiguration = require('./configuration')
 const path = require('upath')
 
-touch(path.resolve(__dirname, '../../test/content'), {}, new Date()).then(result => {
-	const cacheObj = {};
-	console.log(getConfiguration(path.resolve('../../test/content'), '../../test/content/different_dir_test/post.md'))
-	console.log(getConfiguration(path.resolve('../../test/content'), '../../test/content/different_dir_test/post.md', cacheObj))
-	console.log(cacheObj)
-	console.log(getConfiguration(path.resolve('../../test/content'), '../../test/content/different_dir_test/index.md', cacheObj))
-	console.log(cacheObj)
-})
+const cacheObj = {};
+console.log(getConfiguration(path.resolve('../../test/content'), '../../test/content/different_dir_test/post.md'))
+console.log(getConfiguration(path.resolve('../../test/content'), '../../test/content/different_dir_test/post.md', cacheObj))
+console.log(cacheObj)
+console.log(getConfiguration(path.resolve('../../test/content'), '../../test/content/different_dir_test/index.md', cacheObj))
+console.log(cacheObj)
